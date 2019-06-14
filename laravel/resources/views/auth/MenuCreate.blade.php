169804491d25dfda4f14b4ccd0cda40983500355
lxinkai <lxinkai@outlook.com>
2019-06-14 09:43:28 +0800

    修改layui

--- laravel/resources/views/auth/MenuCreate.blade.php
+++ laravel/resources/views/auth/MenuCreate.blade.php
@@ -35,56 +35,23 @@
 
 <body>
 <div class="cBody">
-    <div id="test4" class="demo-transfer"></div>
-    <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
-        <legend>显示搜索框</legend>
-    </fieldset>
+    <div id="test1"></div>
 
     <script>
 
-        layui.use(['transfer', 'layer', 'util'], function(){
-            var $ = layui.$
-                ,transfer = layui.transfer
-                ,layer = layui.layer
-                ,util = layui.util;
+        layui.use('transfer', function() {
+            var transfer = layui.transfer;
 
-            //模拟数据
-            var data1 = [
-                {"value": "1", "title": "李白"}
-                ,{"value": "2", "title": "杜甫"}
-                ,{"value": "3", "title": "苏轼"}
-                ,{"value": "4", "title": "李清照"}
-                ,{"value": "5", "title": "鲁迅", "disabled": true}
-                ,{"value": "6", "title": "巴金"}
-                ,{"value": "7", "title": "冰心"}
-                ,{"value": "8", "title": "矛盾"}
-                ,{"value": "9", "title": "贤心"}
-            ]
-
-                ,data2 = [
-                {"value": "1", "title": "瓦罐汤"}
-                ,{"value": "2", "title": "油酥饼"}
-                ,{"value": "3", "title": "炸酱面"}
-                ,{"value": "4", "title": "串串香", "disabled": true}
-                ,{"value": "5", "title": "豆腐脑"}
-                ,{"value": "6", "title": "驴打滚"}
-                ,{"value": "7", "title": "北京烤鸭"}
-                ,{"value": "8", "title": "烤冷面"}
-                ,{"value": "9", "title": "毛血旺", "disabled": true}
-                ,{"value": "10", "title": "肉夹馍"}
-                ,{"value": "11", "title": "臊子面"}
-                ,{"value": "12", "title": "凉皮"}
-                ,{"value": "13", "title": "羊肉泡馍"}
-                ,{"value": "14", "title": "冰糖葫芦", "disabled": true}
-                ,{"value": "15", "title": "狼牙土豆"}
-            ];
-            //显示搜索框
+            //渲染
             transfer.render({
-                elem: '#test4'
-                ,data: data1
-                ,title: ['文本墨客', '获奖文人']
-                ,showSearch: true
-            })
+                elem: '#test1'  //绑定元素
+                , data: [
+                    {"value": "1", "title": "李白", "disabled": "", "checked": ""}
+                    , {"value": "2", "title": "杜甫", "disabled": "", "checked": ""}
+                    , {"value": "3", "title": "贤心", "disabled": "", "checked": ""}
+                ]
+                , id: 'demo1' //定义索引
+            });
         });
 
     </script>
